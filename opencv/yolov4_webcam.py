9465c452b05681b0aab45cdf164d5f177f173a7c
dschwalm <daniel.schwalm@gmail.com>
2021-04-15 21:18:29 +0200

    Added background color for bounding box title

--- opencv/yolov4_webcam.py
+++ opencv/yolov4_webcam.py
@@ -27,10 +27,15 @@ def get_yolo_preds(net, video_url, confidence_threshold, overlapping_threshold, 
             print("Error opening video stream or file")
             return
 
-        yolo_width_height = (320, 320)
+        yolo_width_height = (416, 416)
+
+        counter = 0
+        max_count = 0
 
         while True:
             (_, frame) = cap.read()
+
+            counter += 1 
             
             if frame_resize_width:
                 frame = imutils.resize(frame, width=frame_resize_width)
@@ -66,10 +71,23 @@ def get_yolo_preds(net, video_url, confidence_threshold, overlapping_threshold, 
                     (x, y) = (boxes[i][0], boxes[i][1])
                     (w, h) = (boxes[i][2], boxes[i][3])
                     color = [int(c) for c in colors[classIDs[i]]]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
                     text = "{}: {:.4f}".format(labels[classIDs[i]], confidences[i])
-                    cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+                    
+                    # draw bounding box title background
+                    text_offset_x = x
+                    text_offset_y = y
+                    text_color = (255, 255, 255)
+                    (text_width, text_height) = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, thickness=1)[0]
+                    box_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width - 80, text_offset_y - text_height + 4))
+                    cv2.rectangle(frame, box_coords[0], box_coords[1], color, cv2.FILLED)
+                    
+                    # draw bounding box title
+                    cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1)
 
+            #if len(bboxes) > max_count:
+            #    max_count = len(bboxes)
+            #    cv2.imwrite('captured_' + str(counter) + '.jpg', frame) 
             cv2.imshow("YOLOv4 Object Detection", frame)
             key = cv2.waitKey(1) & 0xFF
             # if the `q` key was pressed, break the loop
